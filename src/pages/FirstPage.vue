5a27e427f5ec914ebd029b265b262c300e6cda84
Teeraphat Prannok <66160068@go.buu.ac.th>
2025-01-04 16:41:28 +0700

    Add onMounted

--- src/pages/FirstPage.vue
+++ src/pages/FirstPage.vue
@@ -1,9 +1,13 @@
 <template>
   <q-page padding>
     <div>First Page</div>
+    <CounterComponent :num="2"></CounterComponent>
+    <CounterComponent :num="4"></CounterComponent>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import CounterComponent from 'src/components/CounterComponent.vue';
+
 //
 </script>
